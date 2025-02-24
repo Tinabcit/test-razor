9c2b167faffbfc8a1b1a23948eee1bfd3eccbebe
Tina Shabestari <tshabestari@my.bcit.ca>
2025-02-24 12:01:25 -0800

    Initial commit including project folder

---
+++ HelloModel/Hello.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic you need can go here
+        }
+    }
+}
